3f6fe32b897f1db4256724a826c048a1944145ab
Jyoti <jyoti.contact@gmail.com>
2020-02-04 21:26:35 +0000

    update function to respond with an array of tasks

--- handler.js
+++ handler.js
@@ -1,17 +1,45 @@
 'use strict';
-
-module.exports.tasks = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'your API works'
-      },
-      null,
-      2
-    )
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+app.get('/tasks', function(req, res) {
+  res.json({
+    tasks: {
+      custom: [
+        {
+          id: '1',
+          description: 'Create a brag doc',
+          completed: false,
+          points: 5
+        },
+        {
+          id: '2',
+          description: 'Start a new hobby',
+          completed: false,
+          points: 5
+        },
+        {
+          id: '3',
+          description: 'Commission a portrait',
+          completed: false,
+          points: 5
+        }
+      ],
+      random: [
+        {
+          id: '4',
+          description: 'Practice Mindfulness',
+          completed: false,
+          points: 5
+        },
+        {
+          id: '5',
+          description: 'Plan a holiday',
+          completed: false,
+          points: 5
+        }
+      ]
+    }
+  });
+});
+module.exports.tasks = serverless(app);
