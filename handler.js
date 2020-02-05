1ccb165df018feff8b5150b09de265351103bb71
Jyoti <jyoti.contact@gmail.com>
2020-02-05 20:14:53 +0000

    app.post working

--- handler.js
+++ handler.js
@@ -2,6 +2,7 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+
 app.get('/tasks', function(req, res) {
   res.json({
     tasks: {
@@ -42,4 +43,21 @@ app.get('/tasks', function(req, res) {
     }
   });
 });
+
+app.post('/tasks/', function(req, res) {
+  res.send({
+    message: "I don't know what I'm doing"
+  });
+});
+
+// app.get('/tasks/:id', function(req, res) {
+//   res.send({
+//     message: "You're searching for a task?"
+//   });
+// });
+
+// app.delete("/tasks/:id", function (req, res) {
+
 module.exports.tasks = serverless(app);
+module.exports.create = serverless(app);
+// module.exports.find = serverless(app);
