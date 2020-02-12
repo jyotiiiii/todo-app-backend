5fc78e249da62e1e506a0db6146d242a2e5804c5
Jyoti <jyoti.contact@gmail.com>
2020-02-12 23:22:57 +0000

    add database connection

--- handler.js
+++ handler.js
@@ -2,46 +2,28 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'xxxxx',
+  user: 'xxxxx',
+  password: 'xxxxx',
+  database: 'xxxxx'
+});
 
 app.get('/tasks', function(req, res) {
-  res.json({
-    tasks: {
-      custom: [
-        {
-          id: '1',
-          description: 'Create a brag doc',
-          completed: false,
-          points: 5
-        },
-        {
-          id: '2',
-          description: 'Start a new hobby',
-          completed: false,
-          points: 5
-        },
-        {
-          id: '3',
-          description: 'Commission a portrait',
-          completed: false,
-          points: 5
-        }
-      ],
-      random: [
-        {
-          id: '4',
-          description: 'Practice Mindfulness',
-          completed: false,
-          points: 5
-        },
-        {
-          id: '5',
-          description: 'Plan a holiday',
-          completed: false,
-          points: 5
-        }
-      ]
+  connection.query(
+    'SELECT * FROM `task` WHERE `user_id`  = "5cd7209f-b05b-456a-9327-55e8af3f945e"',
+    function(error, results, fields) {
+      // error will be an Error if one occurred during the query
+      if (error) {
+        console.error('Your query had a problem with retrieving tasks', error);
+        res.status(500).json({ errorMessage: error });
+      } else {
+        // Query was successful
+        res.json({ tasks: results });
+      }
     }
-  });
+  );
 });
 
 app.post('/tasks/', function(req, res) {
