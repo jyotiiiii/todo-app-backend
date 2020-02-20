b31390a4cac9efa68ba198aac5360abecbf23c23
Jyoti <jyoti.contact@gmail.com>
2020-02-20 21:47:52 +0000

    Create a new task POST request

--- handler.js
+++ handler.js
@@ -2,7 +2,10 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+app.use(express.json());
+const uuidv4 = require('uuid/v4');
 const mysql = require('mysql');
+
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -27,10 +30,32 @@ app.get('/tasks', function(req, res) {
 });
 
 app.post('/tasks/', function(req, res) {
-  res.send({
-    message: 'This POST request will create a new task'
+  // accept information from the client
+  // about what task is being created
+
+  const tasktoInsert = req.body;
+  tasktoInsert.id = uuidv4();
+  // take that information and pre-populate a SQL insert statement
+  // execute the statement
+  connection.query('INSERT INTO `task` SET ?', tasktoInsert, function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) {
+      console.error(
+        'Your query had a problem with inserting a new task',
+        error
+      );
+      res.status(500).json({ errorMessage: error });
+    } else {
+      // return to the client information about the task that has been created
+      res.json({ task: tasktoInsert });
+    }
   });
 });
+
+// INSERT INTO task VALUES ('0c9341ce-1184-4bd9-b90d-6843034c8fe2', '5cd7209f-b05b-456a-9327-55e8af3f945e', 0, 'Go for a walk in the forest', 0, 15);
 
 app.put('/tasks/:id', function(req, res) {
   res.send({
